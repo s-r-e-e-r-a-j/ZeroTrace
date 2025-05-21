ed45b0d1c04c8f2560bc263336b3cbcb7f102698
Sreeraj <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2025-05-22 00:04:13 +0530

    Update zerotrace.sh

--- ZeroTrace/zerotrace.sh
+++ ZeroTrace/zerotrace.sh
@@ -175,7 +175,7 @@ change_ip_address() {
 
 check_bash() {
   if [ -z "$BASH_VERSION" ]; then
-      printf "\e[31m[!]\e[0m Error: This script must be run with Bash.\n"
+      printf "\033[31m[!]\033[0m Error: This script must be run with Bash.\n"
       exit 1
   fi
 }
